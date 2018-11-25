b02ef7251a68d9d37444c8fe7fd51adc14fab518
Shahar Amram <shramr10@gmail.com>
2018-11-26 00:26:50 +0200

    Working! with the logic in main

--- Java 20554/src/Maman12/question1/Main.java
+++ Java 20554/src/Maman12/question1/Main.java
@@ -1,5 +1,4 @@
 package Maman12.question1;
-import java.awt.Color;
 import javax.swing.*;
 
 public class Main {
@@ -10,11 +9,34 @@ public class Main {
 		PointsJPanel panel = new PointsJPanel();
 		
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setBackground(Color.WHITE);
 	    frame.setSize(400, 400);
 	    frame.setLocation(1000, 100);
 	    frame.add(panel);
 	    frame.setVisible(true);
+	    
+	    while (true) {
+		    String x_str = JOptionPane.showInputDialog("Enter the X coordinate between 0-400");
+		    if (x_str.isEmpty()) {
+		    	JOptionPane.showMessageDialog(null, "Program aborted by user", "Program Aborted", JOptionPane.INFORMATION_MESSAGE);
+		    	System.exit(0);
+		    }
+		    int x = Integer.parseInt(x_str);
+	    	while (x < 0 || x > 400) {
+	    		x = Integer.parseInt(JOptionPane.showInputDialog(null, "X coordinate should be between 0-400", "Wrong number", JOptionPane.WARNING_MESSAGE));
+	    	}
+	    	
+		    String y_str = JOptionPane.showInputDialog("Enter the Y coordinate between 0-400");
+		    if (y_str.isEmpty()) {
+		    	JOptionPane.showMessageDialog(null, "Program aborted by user", "Program Aborted", JOptionPane.INFORMATION_MESSAGE);
+		    	System.exit(0);
+		    }
+		    int y = Integer.parseInt(y_str);
+	    	while (y < 0 || y > 400) {
+	    		y = Integer.parseInt(JOptionPane.showInputDialog(null, "Y coordinate should be between 0-400", "Wrong number", JOptionPane.WARNING_MESSAGE));
+		    }
+	    	
+	    	panel.createPoint(x, y);
+	    }
 	}
 }
 
--- Java 20554/src/Maman12/question1/PointsJPanel.java
+++ Java 20554/src/Maman12/question1/PointsJPanel.java
@@ -4,58 +4,32 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
 
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class PointsJPanel extends JPanel {
 	ArrayList<Point> points_arr = new ArrayList<Point>();
-	Boolean drawGraph = true;
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.BLUE);
-		pointCreate(g);
-//		revalidate();
-		repaint();
-	}
-
-	private void pointCreate(Graphics g) {
-		String x_str = JOptionPane.showInputDialog("Enter the X coordinate between 0-400");
-	    if (x_str.isEmpty()) {
-	    	JOptionPane.showMessageDialog(null, "Program aborted by user", "Program Aborted", JOptionPane.INFORMATION_MESSAGE);
-	    	System.exit(0);
-	    }
-	    int x = Integer.parseInt(x_str);
-    	while (x < 0 || x > 400) {
-    		x = Integer.parseInt(JOptionPane.showInputDialog(null, "X coordinate should be between 0-400", "Wrong number", JOptionPane.WARNING_MESSAGE));
-    	}
-    	
-    	
-	    String y_str = JOptionPane.showInputDialog("Enter the Y coordinate between 0-400");
-	    if (y_str.isEmpty()) {
-	    	JOptionPane.showMessageDialog(null, "Program aborted by user", "Program Aborted", JOptionPane.INFORMATION_MESSAGE);
-	    	System.exit(0);
-	    }
-	    int y = Integer.parseInt(y_str);
-    	while (y < 0 || y > 400) {
-    		y = Integer.parseInt(JOptionPane.showInputDialog(null, "Y coordinate should be between 0-400", "Wrong number", JOptionPane.WARNING_MESSAGE));
-	    }
-    	
-	    points_arr.add(new Point(x, y));
-	    
-	    
 		for (int i = 0; i < points_arr.size(); i++) {
 			if (i == 0)
 				g.fillOval(points_arr.get(i).x, points_arr.get(i).y, 10, 10);
 			else {
 				g.fillOval(points_arr.get(i).x, points_arr.get(i).y, 10, 10);
+				g.setColor(Color.RED);
 				g.drawLine(points_arr.get(i-1).x, points_arr.get(i-1).y, points_arr.get(i).x, points_arr.get(i).y);
 				String distance = String.valueOf(Math.hypot(points_arr.get(i-1).x - points_arr.get(i).x, points_arr.get(i-1).y - points_arr.get(i).y));
+				g.setColor(Color.BLUE);
 				g.drawString(distance, (points_arr.get(i-1).x + points_arr.get(i).x) / 2, (points_arr.get(i-1).y + points_arr.get(i).y) / 2);
 			}
-//			revalidate();
-//			repaint();
 		}
+		repaint();
+	}
+	
+	public void createPoint(int x, int y) {
+		points_arr.add(new Point(x, y));
+		
 	}
 }
